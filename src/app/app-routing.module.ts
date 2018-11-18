43cbf70ed2420a37b1e6c2b3c0c0c87c4ee3a889
superpck <superpck@gmail.com>
2018-11-18 15:24:45 +0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './modules/main/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'}
