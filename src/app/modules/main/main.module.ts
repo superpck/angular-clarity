a9df1207a025cece5f3f0460343367b4400754b3
superpck <superpck@gmail.com>
2018-12-02 13:52:45 +0700

    upgrade to clarity 1.0.0

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ClarityModule, ClrFormsNextModule } from '@clr/angular';
+import { ClarityModule } from '@clr/angular';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
@@ -13,12 +13,15 @@ import { PreferenceComponent } from './preference/preference.component';
 import { HelperModule } from 'src/app/pipes/helpers.module';
 
 @NgModule({
-  declarations: [MainPageComponent, PageNotFoundComponent, AboutComponent, LayoutComponent, UserComponent, PreferenceComponent],
+  declarations: [
+    MainPageComponent, PageNotFoundComponent,
+    AboutComponent, LayoutComponent,
+    UserComponent, PreferenceComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
     ClarityModule,
-    ClrFormsNextModule,
     HelperModule,
     MainRoutingModule
   ]
