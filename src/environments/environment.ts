c2cdb743d15b6d6016b6011468e4ca82ad3f986c
Mr.Pornchai Khamperngjai <superpck@gmail.com>
2018-12-14 12:41:32 +0700

    update to Clarity 1.0.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   appName: 'My Application',
-  version: '0.0.1',
-  subVersion: '2018.11.18-01',
+  version: '0.0.2',
+  subVersion: '2018.12.14-01',
   apiUrl: 'http://localhost:3000',
   whiteUrl: 'http://localhost:3000',
   blackUrl: 'http://localhost:3000/login'
