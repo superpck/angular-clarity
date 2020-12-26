6f0e90b4bd083cb66d3348627d81d6183c1a45b9
Pornchai Khamperngjai <superpck@gmail.com>
2020-12-26 11:46:58 +0700

    export excel

---
+++ src/app/services/excel.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExcelService {
+
+  constructor() { }
+
+  public exportAsExcelFile(json: any[], excelFileName: string, fileNameSuffix=''): void {
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    // console.log('worksheet',worksheet);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    // const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    this.saveAsExcelFile(excelBuffer, excelFileName, fileNameSuffix);
+  }
+
+  private saveAsExcelFile(buffer: any, fileName = 'export', fileNameSuffix=''): void {
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + fileNameSuffix + EXCEL_EXTENSION);
+  }
+
+}
