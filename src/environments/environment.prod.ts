1d1746a390c188fbf6b735d15bfc1393a6d74f1e
Pornchai K <superpck@gmail.com>
2022-06-20 15:38:12 +0700

    angular 14

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   appName: 'My Application',
-  version: '13.3.9',
-  subVersion: '2022.05.21-01',
+  version: '14.0.2',
+  subVersion: '2022.06.20-01',
   apiUrl: 'http://localhost:3000',
   whiteUrl: 'http://localhost:3000',
   blackUrl: 'http://localhost:3000/login'
