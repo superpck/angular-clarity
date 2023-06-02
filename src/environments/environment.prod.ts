1dc4844518c34a6f3143fed496594624acdab5d7
superpck <superpck@gmail.com>
2023-06-02 16:52:21 +0700

    update package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   appName: 'My Application',
-  version: '14.2.5',
-  subVersion: '2022.10.11-01',
+  version: '16.0.4',
+  subVersion: '2023.06.02-01',
   apiUrl: 'http://localhost:3000',
   whiteUrl: 'http://localhost:3000',
   blackUrl: 'http://localhost:3000/login'
